60972ba94c583ca4c17bbd87626daf86cccfa87d
BpAAP <58846215+BpAAP@users.noreply.github.com>
2020-03-06 13:49:25 +0000

    Temperature sensor program now tested and working

--- code/temperature_sensor/temperature_sensor.ino
+++ code/temperature_sensor/temperature_sensor.ino
@@ -4,7 +4,7 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-#define BUS_PIN 4
+#define BUS_PIN 2
 OneWire oneWire(BUS_PIN);
 
 DallasTemperature sensors(&oneWire);
@@ -16,4 +16,15 @@ void initialise_temperature_sensor(){
 float get_temperature(){
   sensors.requestTemperatures();
   return sensors.getTempCByIndex(0);
+}
+
+void setup(){
+  Serial.begin(9600);
+  Serial.println("Serial Started");
+  initialise_temperature_sensor();
+}
+
+void loop(){
+  Serial.println(get_temperature());
+  delay(1000);
 }
